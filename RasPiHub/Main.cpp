ffc4fc33b5f682f09dcbe4aa2f4035723c2dd359
daguglielmo <daguglielmo@hotmail.com>
2017-01-13 08:55:34 +0000

    Added documentation to currently used files

--- RasPiHub/Main.cpp
+++ RasPiHub/Main.cpp
@@ -1,21 +1,21 @@
 #include "MyMqtt.h"
 int main(int argc, char *argv[])
 {
-	class MyMqtt *mqtt;
-	int rc;
+	class MyMqtt *mqtt;	// Declare MyMqtt class
+	int rc;	// Used for deternining if a reconnection is required
 
-	mosqpp::lib_init();
+	mosqpp::lib_init();	// Required before any mosquittopp commands can be issued
 
-	mqtt = new MyMqtt("Hub", "10.0.0.105", 1883);
+	mqtt = new MyMqtt("Hub", "10.0.0.105", 1883);	// Instantiate instance of MyMqtt and declare id, ip address of Broker and port
 
 	while(1){
-		rc = mqtt->loop();
-		if(rc)
+		rc = mqtt->loop();	// Set rc to be the return value of the mosquitto_loop
+		if(rc)	// Determine if reconnection required
 		{
-			mqtt->reconnect();
+			mqtt->reconnect();	// Forces reconnection
 		}
 	}
-	mosqpp::lib_cleanup();
+	mosqpp::lib_cleanup();	// Releases mosquitopp resources
 
 	return 0;
 }
--- RasPiHub/MyMqtt.cpp
+++ RasPiHub/MyMqtt.cpp
@@ -7,7 +7,7 @@
 MyMqtt::MyMqtt(const char *id, const char *host, int port) : mosquittopp(id)
 {
 	int keepalive = 60;
-	connect(host, port, keepalive);
+	connect(host, port, keepalive);	// Connect to designated broker and set keep alive
 }
 
 MyMqtt::~MyMqtt()
@@ -16,34 +16,23 @@ MyMqtt::~MyMqtt()
 
 void MyMqtt::on_connect(int rc)
 {
-	printf("Connected with code %d.\n", rc);
-	if(rc == 0)
+	printf("Connected with code %d.\n", rc);	// Prints connection code. Should be 0
+	if(rc == 0)	// Sucess
 	{
                 /* Only attempt to subscribe on a successful connect. */
-                subscribe(NULL, "Hub/#");
+                subscribe(NULL, "Hub/#");	// Subscribe to all Hub messages
 	}
 }
 
 void MyMqtt::on_message(const struct mosquitto_message *message)
 {
-/*        double temp_celsius, temp_farenheit;
-        char buf[51];
-
-        if(!strcmp(message->topic, "Thermostat/Change/TempUp")){
-                memset(buf, 0, 51*sizeof(char));
-                /* Copy N-1 bytes to ensure always 0 terminated. /
-                memcpy(buf, message->payload, 50*sizeof(char));
-                temp_celsius = atof(buf);
-                temp_farenheit = temp_celsius*9.0/5.0 + 32.0;
-                snprintf(buf, 50, "%f", temp_farenheit);
-                publish(NULL, "temperature/farenheit", strlen(buf), buf);*/
-	char* topic = (char*)message->topic;
-	char* payload = (char*)message->payload;
-	printf("Topic: %s\n", topic);
-	printf("Message: %s\n", payload);
+	char* topic = (char*)message->topic;	// Assign topic to char pointer for display
+	char* payload = (char*)message->payload;	// Assign payload to char pointer for display
+	printf("Topic: %s\n", topic);	// Display topic
+	printf("Message: %s\n", payload);	// Display payload
 }
 
 void  MyMqtt::on_subscribe(int mid, int qos_count, const int *granted_qos)
 {
-	printf("Subscription succeeded.\n");
+	printf("Subscription succeeded.\n");	// Print on sucessful subscription
 }
